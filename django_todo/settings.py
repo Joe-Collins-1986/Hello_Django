33b17467b47a7c0fe65697adb3905decc38a6b31
Joe-Collins-1986 <collins.jw@btinternet.com>
2022-12-13 18:51:38 +0000

    fixed allowed hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['django-todo-app-for-testing.herokuapp.com']
 
 
 # Application definition
